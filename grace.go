f7eee9a1b1317c6c07518a931c46fc23a4bb3f2c
Mattia Barbon <mattia.barbon@booking.com>
2020-03-08 12:41:02 +0100

    Switch to Go modules

---
+++ grace.go
@@ -0,0 +1,6 @@
+// dummy file to make build succeed; without this:
+//
+//   $ go build
+//   can't load package: package github.com/bookingcom/grace: unknown import path "github.com/bookingcom/grace": cannot find module providing package github.com/bookingcom/grace
+//
+package grace
